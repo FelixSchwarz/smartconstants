427ea731abd3b9200d90ff187405f74668d96cda
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-08-21 11:49:37 +0200

    bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def requires_from_file(filename):
 
 setup(
     name = 'SmartConstants',
-    version = '1.2.0',
+    version = '1.3.0',
     description = 'magic class to declare easy-to-use "enum"-like values',
     long_description=(read('Changelog.txt')),
 
